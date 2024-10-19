f1f60000878fb289714f18e77e3925964e37a3e7
aviareps_pwolf <pwolf@aviareps.com>
2024-10-19 12:41:32 +0200

    email spam info added

--- components/global/EmailInfo.tsx
+++ components/global/EmailInfo.tsx
@@ -4,7 +4,7 @@ import {useEffect} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faClose} from "@fortawesome/free-solid-svg-icons";
 
-const EmailInfo = ({setEmailInfo}:{setEmailInfo:(boolean)=>void}) => {
+const EmailInfo = ({setEmailInfo}:{setEmailInfo:(data:boolean)=>void}) => {
 
 
     useEffect(()=>{
