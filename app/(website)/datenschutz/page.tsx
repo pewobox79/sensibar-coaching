bd9199fefec4b716a9f60b4f55b75c9fcc7d2cdf
aviareps_pwolf <pwolf@aviareps.com>
2024-11-04 10:59:38 +0100

    fallback

--- app/(website)/datenschutz/page.tsx
+++ app/(website)/datenschutz/page.tsx
@@ -1,12 +1,13 @@
 import {getBasicPageContent} from "@/lib/strapi/generalHelper";
 import RenderContentHelper from "@/components/strapi/RenderContentHelper";
+import {Suspense} from "react";
 
 const Datenschutz =async ()=>{
 
     const data = await getBasicPageContent("Datenschutzerklärung")
     return <>
-        <h1>{ data.data ? data?.data[0]?.title:"" }</h1>
-        <RenderContentHelper blocks={ data.data ? data.data[0].mainContent: [] }/></>
+        <h1>{ data.data ? data?.data[0]?.title:"title" }</h1>
+        <Suspense fallback={"loading"}><RenderContentHelper blocks={ data.data ? data.data[0].mainContent: [] }/></Suspense></>
 }
 
 export default Datenschutz;
