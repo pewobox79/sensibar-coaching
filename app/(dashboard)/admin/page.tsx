5295a90dbad5852fe7d3b237f10293b3444a98b8
aviareps_pwolf <pwolf@aviareps.com>
2024-11-08 08:14:20 +0100

    updated saving logic workshops

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,18 +1,10 @@
-'use client'
 
 import AdminPage from "@/pages/AdminPage";
-import {useClientStore} from "@/stores/useClientStore";
 
 const Admin =()=>{
-const clientStore = useClientStore()
-    const clientData = clientStore?.clientData
+
     return <div>
-        <h1>Admin page</h1>
         <AdminPage/>
-        <p>Firstname: {clientData?.personalData.firstname}</p>
-        <p>Lastname: {clientData?.personalData.lastname}</p>
-        <p>Email: {clientData?.contact.email}</p>
-        {/* add more fields if needed */}
     </div>
 }
 
