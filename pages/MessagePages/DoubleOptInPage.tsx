aed2984b3d7e662020c79c64224dac4ac5c47ea1
aviareps_pwolf <pwolf@aviareps.com>
2024-11-09 17:26:25 +0100

    updated mailing configurations

--- pages/MessagePages/DoubleOptInPage.tsx
+++ pages/MessagePages/DoubleOptInPage.tsx
@@ -2,22 +2,32 @@
 
 import {useSearchParams} from "next/navigation";
 import {useEffect} from "react";
-import {executeDoubleOptIn} from "@/lib/strapi/workshopHelper";
+import {executeDoubleOptIn, getSingleWorkshop} from "@/lib/strapi/workshopHelper";
 
 const DoubleOptInPage = () => {
 
     const searchParams = useSearchParams()
     const id = searchParams ? searchParams.get("id") : "";
     const type = searchParams ? searchParams.get("type") : "";
+    const workshopId = searchParams ? searchParams.get("wsId") : "";
 
     useEffect(() => {
+        getSingleWorkshop(workshopId).then((data)=>{
 
-            executeDoubleOptIn(id).then(data => console.log(data));
+            console.log("data workshop", data.data)
+
+            const workshopLink = data.data?.link?.href;
+            const {title, workshop_date} = data?.data
+
+            executeDoubleOptIn(id, workshopLink, title, workshop_date).then(data => console.log(data));
+        })
+
+
         },
         [id]
     )
     return <>
-        <h3>Deine Anmeldung zu meinen { type }. </h3>
+        <h3>Deine Anmeldung zu meinen Workshop. </h3>
         <p>Ich freue mich über Dein Kommen.</p>
         <p>Du bekommst alle nötigen Informationen in den nächsten Minuten per Email</p>
 
