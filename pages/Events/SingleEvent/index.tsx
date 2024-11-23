a7437a5898d93ed22f2e14785fa2afd50ad70e4c
aviareps_pwolf <pwolf@aviareps.com>
2024-11-23 22:23:29 +0100

    anmeldeformular nur bei bestätigten events sichtbar

--- pages/Events/SingleEvent/index.tsx
+++ pages/Events/SingleEvent/index.tsx
@@ -4,12 +4,11 @@ import EventRegistration from "@/components/forms/EventRegistration";
 import EventInfoBox from "@/components/forms/EventRegistration/EventInfoBox";
 import RichTextRenderer from "@/components/strapi/RenderContentHelper";
 
-const SingleEvent = (props:{data:{title:string, description:[],workshop_date:string, workshopTimeStart:string, documentId:string, id:string }}) => {
+const SingleEvent = (props:{data:{title:string, description:[],workshop_date:string,ws_status: string, workshopTimeStart:string, documentId:string, id:string }}) => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     //@ts-expect-error
-    const workshopDetails:{title: string, description: [], workshop_date:string, workshopTimeStart: string, workshopTimeEnd: string, type:string, documentId:string} = props.data ? props?.data:{}
+    const workshopDetails:{title: string, description: [], workshop_date:string, workshopTimeStart: string, workshopTimeEnd: string, type:string, documentId:string, ws_status:string} = props.data ? props?.data:{}
 
-    console.log("workshop details", workshopDetails)
     return <div className={ styles.singleEventWrapper }>
         <div className={ styles.singleEventInner }>
             <div className={ styles.singleEventHeader }>
@@ -24,7 +23,7 @@ const SingleEvent = (props:{data:{title:string, description:[],workshop_date:str
                 </div>
                 <div className={ styles.singleEventContent50 }>
                     <EventInfoBox workshop_date={workshopDetails.workshop_date} workshopTimeStart={workshopDetails.workshopTimeStart} workshopTimeEnd={workshopDetails.workshopTimeEnd} workshopType={workshopDetails.type}/>
-                    <EventRegistration workshopId={workshopDetails.documentId} workshopName={workshopDetails.title}/></div>
+                    {workshopDetails.ws_status === "confirmed" &&<EventRegistration workshopId={workshopDetails.documentId} workshopName={workshopDetails.title}/>}</div>
             </div>
             </div>
 
