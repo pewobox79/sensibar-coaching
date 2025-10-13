4e7165b28001db9cab2fd22823042222c2f75a0c
aviareps_pwolf <pwolf@aviareps.com>
2025-10-13 09:41:08 +0200

    updated logo

--- pagesComponents/Homepage.tsx
+++ pagesComponents/Homepage.tsx
@@ -1,11 +1,11 @@
 
 import Image from "next/image";
-import SensibarLogo from "@/assets/images/sensibar-coaching.png"
+import SensibarLogo from "@/assets/images/sensibar_logo_new.png"
 
 const Homepage=()=>{
 
 
-    return <Image src={SensibarLogo["src"]} alt={"senisbar logo"} width={300} height={300}/>
+    return <Image src={SensibarLogo["src"]} alt={"senisbar logo"} width={500} height={500}/>
 
 }
 
