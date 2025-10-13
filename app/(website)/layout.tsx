135883230de5a3e805a362291bb904a2dde1a896
aviareps_pwolf <pwolf@aviareps.com>
2025-10-13 11:17:26 +0200

    clean up

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,55 +1,13 @@
 import type {Metadata, Viewport} from "next";
-import localFont from "next/font/local";
 import "../../styles/globals.css";
 import MainLayout from "@/layouts/MainLayout";
 import PageLayout from "@/layouts/PageLayout";
 import Consent from "@/components/Consent";
 import { Poppins, Montserrat } from 'next/font/google'
 
-
-/*const roxborough = localFont({
-    src: [
-        {
-            path: '../../assets/fonts/roxborough/roxborough-cf-regular.woff',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: '../../assets/fonts/roxborough/roxborough-cf-regular.woff',
-            weight: '600',
-            style: 'bold',
-        },
-    ],
-    variable: "--font-roxborough-cf-regular",
-});
-
-const TTNorms = localFont({
-    src: [
-        {
-            path: '../../assets/fonts/tt_norms/TTNorms-Regular.otf',
-            weight: '400',
-            style: 'lighter',
-        }, {
-            path: '../../assets/fonts/tt_norms/TTNorms-Medium.otf',
-            weight: '600',
-            style: 'normal',
-        },
-        {
-            path: '../../assets/fonts/tt_norms/TTNorms-Bold.otf',
-            weight: '900',
-            style: 'bold',
-        }, {
-            path: '../../assets/fonts/tt_norms/TTNorms-Italic.otf',
-            weight: '400',
-            style: 'italic',
-        },
-    ],
-        variable: "--font-tt-norms",
-});*/
-
 const poppins = Poppins({
     subsets: ['latin'],
-    weight: ['400', '600', '700'], // nur die benötigten Gewichtungen importieren
+    weight: ['400', '600', '700'],
     variable: '--font-poppins',
     display: 'swap',
 })
