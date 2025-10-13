320b3d4f544e569831981314b15326166b99fb7e
aviareps_pwolf <pwolf@aviareps.com>
2025-10-13 20:05:44 +0200

    location updated

--- components/forms/EventRegistration/EventInfoBox.tsx
+++ components/forms/EventRegistration/EventInfoBox.tsx
@@ -14,14 +14,14 @@ type EventInfoTypes ={
     location: EventDetailsProps['location']
 }
 const EventInfoBox =({workshop_date, workshopTimeStart, workshopTimeEnd, workshopType, location}:EventInfoTypes )=>{
-
+console.log("props event info", location, workshopType)
     const addressData = `${location.street} ${location.streetNumber}, ${location.zipCode} ${location.city} `
 
     return <div className={styles.singleEventDetailsBoxWrapper}>
         <div className={styles.detailsBoxItemHeader}><h3>Event Details</h3></div>
         <div className={styles.detailsBoxItem}><i><FontAwesomeIcon icon={faCalendar} style={{width:' 40px'}}/></i> {workshop_date} </div>
         <div className={styles.detailsBoxItem}><i><FontAwesomeIcon icon={faClock} style={{width:' 40px'}}/></i> {formatTimeToAdminFormat(workshopTimeStart as string)} bis {formatTimeToAdminFormat(workshopTimeEnd as string)} Uhr </div>
-        {workshopType != "inPerson" && <div className={styles.detailsBoxItem}><i><FontAwesomeIcon icon={faLocationDot} style={{width:' 40px'}}/></i>{addressData}</div>}
+        <div className={styles.detailsBoxItem}><i><FontAwesomeIcon icon={faLocationDot} style={{width:' 40px'}}/></i>{workshopType != "online" ? addressData: workshopType}</div>
     </div>
 }
 
