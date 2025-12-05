611fbf481e636c0d4633a06772d11f4f9a4f1583
aviareps_pwolf <pwolf@aviareps.com>
2025-12-05 12:16:54 +0100

    updated nextjs to 16, nodemailer update 7.0.11 and removed mongoose

--- layouts/AuthLayout.tsx
+++ layouts/AuthLayout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const AuthLayout =async ({children}:{children: React.ReactNode | React.ReactElement |React.ReactElement[]})=>{
 
-    const cookieStore = cookies()
+    const cookieStore = await cookies()
     const token:{name: string, value: string} |undefined = cookieStore.get('sensiToken')
 
     if(!token?.value){
