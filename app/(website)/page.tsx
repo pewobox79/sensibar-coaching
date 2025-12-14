0b1ac1fd5e30ce30d4959f64a81d21dce2711137
aviareps_pwolf <pwolf@aviareps.com>
2025-12-14 20:43:19 +0100

    addd maintanence

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -5,7 +5,7 @@ import Container from "@/components/global/Container";
 
 export default async function Home() {
 
-  const isMaintanence = true
+  const isMaintanence = false
   if(isMaintanence) return <Container id={"kajd"}><div style={{height: '100vh', display: "flex", justifyContent: "center", alignItems: "center", color: 'black'}}><h3>Seite befindet sich im Umbau</h3></div></Container>
   const homepageData = await getHomepage()
   console.log("homepagedata", homepageData)
