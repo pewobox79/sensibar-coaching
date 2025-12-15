1b11a3da8b049616732ab7328225a26046404b9c
aviareps_pwolf <pwolf@aviareps.com>
2025-12-15 16:34:39 +0100

    maintenance mode changed

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -5,8 +5,8 @@ import Container from "@/components/global/Container";
 
 export default async function Home() {
 
-  const isMaintanence = true
-  if(isMaintanence) return <Container id={"kajd"}><div style={{height: '100vh', display: "flex", justifyContent: "center", alignItems: "center", color: 'black'}}><h3>Seite befindet sich im Umbau</h3></div></Container>
+  const isMaintenance = false
+  if(isMaintenance) return <Container id={"kajd"}><div style={{height: '100vh', display: "flex", justifyContent: "center", alignItems: "center", color: 'black'}}><h3>Seite befindet sich im Umbau</h3></div></Container>
   const homepageData = await getHomepage()
   return (
      <Homepage data={homepageData?.data}/>
