909da7347c28651268fe53741a7356e8b8cac822
aviareps_pwolf <pwolf@aviareps.com>
2025-12-15 16:42:42 +0100

    text color jumbotron

--- components/fontendEndComponents/BackgroundImgWithText.tsx
+++ components/fontendEndComponents/BackgroundImgWithText.tsx
@@ -3,18 +3,15 @@ import styles from '@/styles/BackgroundImgWithText.module.css'
 import RichTextRenderer from "@/components/strapi/RenderContentHelper";
 import {JumbotronType, TextBlock} from "@/types/generalTypes";
 import BackgroundImage from "@/components/global/BackgroundImage";
-import {getColor} from "@/utils/helper/colorHelper";
 
 
 const BackgroundImgWithText=({data}:{data: JumbotronType})=>{
 
-    console.log("data", data)
-    const colorSet = getColor(data?.bgColor?.color as string)
     return <Container id='jumbotron'>
         <div className={styles.backgroundImgWithTextInner}>
             <BackgroundImage imageUrl={data?.Image?.url as string}/>
             <div className={styles.textBlock}>
-                <RichTextRenderer blocks={data.text as TextBlock} textColor={colorSet.color}/>
+                <RichTextRenderer blocks={data.text as TextBlock} textColor={'white'}/>
             </div>
 
         </div>
