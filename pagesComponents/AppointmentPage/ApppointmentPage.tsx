e5359b72f1a9d8785aa38b46e29c963af5dcfa78
aviareps_pwolf <pwolf@aviareps.com>
2026-02-16 16:14:22 +0100

    redirect update

--- pagesComponents/AppointmentPage/ApppointmentPage.tsx
+++ pagesComponents/AppointmentPage/ApppointmentPage.tsx
@@ -1,10 +1,13 @@
 import {getPage} from "@/lib/strapi/generalHelper";
 import StrapiItemsRendering from "@/layouts/StrapiItemsRendering";
 import {StrapiData} from "@/types/generalTypes";
+import {redirect} from "next/navigation";
 
 const AppointmentPage = async ({slug}: { slug: string }) => {
     const pageData = await getPage(slug)
     const selectedPage = pageData.data[0] as StrapiData
+
+    if(!selectedPage) return redirect("/")
     return (
         <div>
             { selectedPage?.items?.map(item => <StrapiItemsRendering key={ item.id } { ...item }/>) }
