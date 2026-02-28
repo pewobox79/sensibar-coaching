103e63afeeec4c603c4d01a0b96fc72ff122a3fe
aviareps_pwolf <pwolf@aviareps.com>
2026-02-28 07:57:02 +0100

    updated helper

--- lib/pdfCreator/helper.tsx
+++ lib/pdfCreator/helper.tsx
@@ -2,21 +2,37 @@ export function getResultDescription(title: string) {
 
     if (title.includes('Wenig')) {
 
-        return 'Du zeigst nur wenige Merkmale, die typisch für Hochsensibilität sind. Wahrscheinlich reagierst du durchschnittlich auf Reize, Emotionen und soziale Situationen.'
+        return {
+            description: 'Du zeigst nur wenige Merkmale, die typisch für Hochsensibilität sind. Wahrscheinlich reagierst du durchschnittlich auf Reize, Emotionen und soziale Situationen.',
+            cta: 'das ist der text für wenig anzeichen'
+        }
     }
 
     if (title.includes("einige")) {
 
-        return 'Du erkennst dich in einigen Aspekten wieder, z.B. bei emotionaler oder sensorischer Sensibilität Möglicherweise bist du in bestimmten Lebensbereichen empfindsamer als andere.'
+        return {
+            description: 'Du erkennst dich in einigen Aspekten wieder, z.B. bei emotionaler oder sensorischer Sensibilität Möglicherweise bist du in bestimmten Lebensbereichen empfindsamer als andere.',
+            cta: 'das ist der text für einige anzeichen'
+        }
     }
 
     if (title.includes("stark")) {
 
-        return 'Du bist in nahezu allen Bereichen hochsensibel. Du brauchst wahrscheinlich regelmäßig Rückzugsorte und besondere Strategien im Alltag, um dich nicht zu überfordern.'
+        return {
+            description: 'Du bist in nahezu allen Bereichen hochsensibel. Du brauchst wahrscheinlich regelmäßig Rückzugsorte und besondere Strategien im Alltag, um dich nicht zu überfordern.',
+            cta: 'das ist der text für starke anzeichen'
+        }
     }
 
     if (title.includes("wahrscheinlich")) {
 
-        return 'Viele Aussagen treffen auf dich zu. Du nimmst deine Umwelt wahrscheinlich tiefgründig und intensiv wahr. Achtsamkeit und Selbstfürsorge sind besonders wichtig für dein Wohlbefinden.'
+        return {
+            description: 'Viele Aussagen treffen auf dich zu. Du nimmst deine Umwelt wahrscheinlich tiefgründig und intensiv wahr. Achtsamkeit und Selbstfürsorge sind besonders wichtig für dein Wohlbefinden.',
+            cta: 'das ist der text für wahrscheinlich anzeichen'
+        }
+    }
+    return {
+        description: "",
+        cta: ""
     }
 }
