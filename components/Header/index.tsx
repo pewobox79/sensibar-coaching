17aea7da8760d828b123d097b05aa97175a5e9fb
aviareps_pwolf <pwolf@aviareps.com>
2026-03-03 17:46:10 +0100

    updated changes

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,6 +2,7 @@ import Navigation from "@/components/Navigation";
 import styles from '@/styles/Header.module.css'
 import Image from "next/image";
 import Logo from "@/assets/images/sensibar-coaching-logo-neu.png";
+import Link from "next/link";
 
 
 
@@ -9,7 +10,7 @@ import Logo from "@/assets/images/sensibar-coaching-logo-neu.png";
 const Header = () => {
 
     return <header className={styles.headerElement}>
-        <Image className={styles.logo} src={Logo.src} alt="Logo" width={300} height={110}/>
+        <Link href={"/"}><Image className={styles.logo} src={Logo.src} alt="Logo" width={300} height={110}/></Link>
         <Navigation/>
 
     </header>
