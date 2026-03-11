196da7e4acf79aff9cb66ec134ce29f344fe75a8
aviareps_pwolf <pwolf@aviareps.com>
2026-03-11 16:23:39 +0100

    paypal updated

--- components/PayPal/PayPalBtn.tsx
+++ components/PayPal/PayPalBtn.tsx
@@ -1,24 +1,30 @@
 'use client'
 import {
+    OnApproveDataOneTimePayments,
     PayPalOneTimePaymentButton,
 } from "@paypal/react-paypal-js/sdk-v6";
 
 const PayPalBtn = ({enabled}:{enabled: boolean}    ) => {
-
     return <div
         style={ {
             pointerEvents: enabled ? "auto" : "none",
             opacity: enabled ? 1 : 0.5
         } }
     ><PayPalOneTimePaymentButton
-        createOrder={ async () => {
-            const res = await fetch("/api/orders", {method: "POST"});
-            const order = await res.json();
-            return {orderId: order.id};
-        } }
-        onApprove={ async ({orderId}) => {
-            await fetch(`/api/orders/${ orderId }/capture`, {method: "POST"});
-        } }
+        presentationMode="popup"
+        createOrder={async () => {
+            const response = await fetch("/api/create-order", {
+                method: "POST",
+            });
+            const { orderId } = await response.json();
+            return { orderId };
+        }}
+        onApprove={async ({ orderId }: OnApproveDataOneTimePayments) => {
+            await fetch(`/api/capture-order/${orderId}`, {
+                method: "POST",
+            });
+            console.log("Payment captured!");
+        }}
     /></div>
 };
 
