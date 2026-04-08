f917ab204953e3239e72f25033749aa96a5a7ee3
aviareps_pwolf <pwolf@aviareps.com>
2026-04-08 12:01:52 +0200

    testing

--- app/(website)/widerrufsbelehrung/page.tsx
+++ app/(website)/widerrufsbelehrung/page.tsx
@@ -33,9 +33,9 @@ export const metadata: Metadata = {
 const Widerrufsbelehrung = async () => {
 
     const data = await getBasicPageContent("widerrufsbelehrung")
+    console.log("data widerruf", data)
 
     return <div className={ "innerWrapper" }>
-
 
         <Suspense fallback={ "loading" }><h1>{ data?.data ? data?.data[0]?.title : "" }</h1><RenderContentHelper
             blocks={ data?.data ? data?.data[0]?.mainContent : [] }/></Suspense></div>
